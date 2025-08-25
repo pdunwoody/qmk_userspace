9f91d68369348bec59e93af98cd665c1e0789e62
Paul Dunwoody <paul.dunwoody@cwaengineers.com>
2025-08-25 20:19:34 +0000

    Update rules.mk to comment out MCU and BOOTLOADER options

--- keyboards/crkbd/rev1/keymaps/pdunwoody/rules.mk
+++ keyboards/crkbd/rev1/keymaps/pdunwoody/rules.mk
@@ -1,8 +1,8 @@
 # Build Options
 #   change yes to no to disable
 #
-MCU = RP2040
-BOOTLOADER = rp2040
+#MCU = RP2040
+#BOOTLOADER = rp2040
 # CONVERT_TO = promicro_rp2040
 
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
